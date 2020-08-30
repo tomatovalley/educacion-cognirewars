66f31a1604b048c7a600e3fc5b9de5db2f5fe44d
Patricia  Collado <patricia.colladom@gmail.com>
2020-08-30 11:21:59 -0500

    Template cursos y actividades

---
+++ src/components/layout/ActividadLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-appBar app color="white" dark absolute>
+      <div>
+        Hol
+      </div>
+    </v-appBar>
+    <v-content>
+      <v-col align="center" justify="center">
+        <router-view />
+      </v-col>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "ActividadLayout",
+};
+</script>
---
+++ src/views/Actividades.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-form>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <div>
+            <h2 class="mb-6">Crear actividad</h2>
+          </div>
+          <v-text-field
+            v-model="actividad"
+            label="actividad"
+            placeholder="Nombre de la actividad"
+            single-line
+            solo
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="descripcion"
+            label="descripcion"
+            placeholder="Descripcion de la actividad"
+            single-line
+            solo
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="fecha"
+            label="fecha"
+            placeholder="Disponible hasta"
+            single-line
+            solo
+            required
+          ></v-text-field>
+          <div class="text-center">
+            <v-btn class="mb-2" color="error" @click="submit">
+              Crear curso
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
